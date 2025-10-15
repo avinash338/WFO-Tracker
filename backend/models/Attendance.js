b7a7ecd572d97d00c11facbfd8f91258997e796d
Avinash <42925371+avinash338@users.noreply.github.com>
2025-10-15 22:13:36 +0530

    Update Attendance.js

--- backend/models/Attendance.js
+++ backend/models/Attendance.js
@@ -1,22 +1,22 @@
-// const mongoose = require("mongoose");
-
-// const AttendanceSchema = new mongoose.Schema({
-//   userId: { type: String, required: true },
-//   userEmail: { type: String, required: true },
-//   date: { type: String, required: true },
-//   status: { type: String, enum: ["WFO", "WFH"], required: true },
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("Attendance", AttendanceSchema);
-
-
 const mongoose = require("mongoose");
 
 const AttendanceSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  userEmail: { type: String, required: true, lowercase: true, trim: true },
-  date: { type: Date, required: true },
+  userId: { type: String, required: true },
+  userEmail: { type: String, required: true },
+  date: { type: String, required: true },
   status: { type: String, enum: ["WFO", "WFH"], required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Attendance", AttendanceSchema);
+
+
+// const mongoose = require("mongoose");
+
+// const AttendanceSchema = new mongoose.Schema({
+//   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+//   userEmail: { type: String, required: true, lowercase: true, trim: true },
+//   date: { type: Date, required: true },
+//   status: { type: String, enum: ["WFO", "WFH"], required: true },
+// }, { timestamps: true });
+
+// module.exports = mongoose.model("Attendance", AttendanceSchema);
