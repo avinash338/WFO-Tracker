ac655b6fc68c7f88255e25411d37d4d76f512a5c
Avinash <42925371+avinash338@users.noreply.github.com>
2025-10-15 21:55:30 +0530

    Update Attendance.js

--- backend/models/Attendance.js
+++ backend/models/Attendance.js
@@ -1,9 +1,21 @@
+// const mongoose = require("mongoose");
+
+// const AttendanceSchema = new mongoose.Schema({
+//   userId: { type: String, required: true },
+//   userEmail: { type: String, required: true },
+//   date: { type: String, required: true },
+//   status: { type: String, enum: ["WFO", "WFH"], required: true },
+// }, { timestamps: true });
+
+// module.exports = mongoose.model("Attendance", AttendanceSchema);
+
+
 const mongoose = require("mongoose");
 
 const AttendanceSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  userEmail: { type: String, required: true },
-  date: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  userEmail: { type: String, required: true, lowercase: true, trim: true },
+  date: { type: Date, required: true },
   status: { type: String, enum: ["WFO", "WFH"], required: true },
 }, { timestamps: true });
 
